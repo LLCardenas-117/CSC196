6e0b0ed3e8159b9ecc0b5afa1d01ce58abdb5f04
LLCardenas-117 <lcardenas@student.neumont.edu>
2025-07-01 10:17:23 -0600

    Added core files for the engine, and added code to the main.cpp

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,4 +1,10 @@
+#include "Core/Math.h"
 #include <iostream>
+
+#define NAME "Landon\n"
+
 int main() {
+	std::cout << NAME;
 	std::cout << "Hello, World!\n";
+	std::cout << errera::pi << std::endl;
 }
